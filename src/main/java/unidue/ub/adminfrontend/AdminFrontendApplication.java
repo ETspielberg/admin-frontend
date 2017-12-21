d8e274cb622570ba6f0d44542eb982d9e1d680e5
Eike Spielberg <Eike.Spielberg@uni-due.de>
2017-12-21 09:13:31 +0100

    initial commit

---
+++ src/main/java/unidue/ub/adminfrontend/AdminFrontendApplication.java
@@ -0,0 +1,24 @@
+package unidue.ub.adminfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class AdminFrontendApplication extends WebSecurityConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminFrontendApplication.class, args);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().disable().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		http.authorizeRequests().anyRequest().authenticated();
+	}
+}
