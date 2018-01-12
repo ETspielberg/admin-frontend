a83aeb20a2d42457cf9df46ea5bbb6f9936eacd0
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-01-12 11:19:04 +0100

    buggy run version

--- src/main/java/unidue/ub/adminfrontend/AdminFrontendApplication.java
+++ src/main/java/unidue/ub/adminfrontend/AdminFrontendApplication.java
@@ -2,6 +2,7 @@ package unidue.ub.adminfrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 @Controller
+@EnableEurekaClient
 public class AdminFrontendApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
